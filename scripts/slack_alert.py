e61a65a95ae35230c51282d94006719e8e9be576
Joebaho <josephmbatchou2@gmail.com>
2026-05-08 21:32:35 -0700

    Adding docker and git repo

--- scripts/slack_alert.py
+++ scripts/slack_alert.py
@@ -1,7 +1,7 @@
 import requests
 
 # Replace with your actual Slack webhook URL
-SLACK_WEBHOOK = "https://hooks.slack.com/services/YOUR/WEBHOOK/URL"
+SLACK_WEBHOOK = "https://hooks.slack.com/services/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"
 
 data = {
     "text": "🚨 AGE-APP Alert: High CPU detected or Deployment Issue!"
